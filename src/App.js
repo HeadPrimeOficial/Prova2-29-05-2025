363fbaa53491d4819e98118dc01e07bd168e4674
Alexandre <154011333+xandde@users.noreply.github.com>
2025-06-08 20:37:20 -0300

    ok

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+/* import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import CadastroPage from './pages/CadastroPage'
 import CardapioPage from './pages/CardapioPage'
@@ -13,4 +13,32 @@ export default function AppRoutes() {
       </Routes>
     </BrowserRouter>
   )
+}*/
+
+// App.js
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import CadastroPage from './pages/CadastroPage';
+import CardapioPage from './pages/CardapioPage';
+import { useState } from 'react';
+
+export default function AppRoutes() {
+  const [pratoEditando, setPratoEditando] = useState(null);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route 
+          path="/cadastro" 
+          element={<CadastroPage pratoEditando={pratoEditando} limparEdicao={() => setPratoEditando(null)} />} 
+        />
+        <Route 
+          path="/cardapio" 
+          element={<CardapioPage onEditar={setPratoEditando} />} 
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 }
