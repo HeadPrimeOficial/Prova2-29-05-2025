43c7d8f9e1a0025f0c5d2694cae591dbf711b31d
Alexandre <154011333+xandde@users.noreply.github.com>
2025-06-09 20:10:10 -0300

    ok

--- src/main/java/com/example/aula/config/CorsConfig.java
+++ src/main/java/com/example/aula/config/CorsConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class CorsConfig {
 
 
